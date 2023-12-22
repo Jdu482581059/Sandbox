3e03bd819d87d0864932f577579f3a5382eeef88
Jdu482581059 <119709427+Jdu482581059@users.noreply.github.com>
2023-12-22 01:53:24 +0000

    added

---
+++ deploy.ps1
@@ -0,0 +1,23 @@
+# Variables
+$subscriptionId = "01312e25-c8f2-49dc-a847-e994d5d4729b"
+$resourceProvidersToRegister = @("Microsoft.Network", "Microsoft.App") # Add your resource providers here
+
+# Connect to Azure account
+Connect-AzAccount
+
+# Set the subscription context
+Set-AzContext -Subscription $subscriptionId
+
+# Get currently registered resource providers
+$registeredProviders = Get-AzResourceProvider -ListAvailable | Select-Object ProviderNamespace
+
+# Iterate through the resource providers to register
+foreach ($provider in $resourceProvidersToRegister) {
+    if ($registeredProviders -notcontains $provider) {
+        # Register the resource provider
+        Register-AzResourceProvider -ProviderNamespace $provider
+        Write-Output "Registered resource provider: $provider"
+    } else {
+        Write-Output "Resource provider $provider is already registered."
+    }
+}
